870142ded6ff4a11c848c0e24256fcca9b230f97
bieye250 <beiye250@gmail.com>
2025-02-27 21:30:20 +0800

    project 1ec

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -8,34 +8,37 @@ import student.StudentArrayDeque;
 
 public class TestArrayDequeEC {
 
+    private final int FIRST = 0;
+
     @Test
     public void testStudent() {
         var deque = new RecordDeque<String>();
         var a = new StudentArrayDeque<Integer>();
         var b = new ArrayDequeSolution<Integer>();
 
-        for (int i = 0; i < 5000; i++) {
-            int rand = (int) (StdRandom.random() * 10);
-            if (rand > 8) {
-                deque.addLast("addLast("+i+")\n");
+        for (int i = 0; i < 100; i++) {
+            int rand = StdRandom.uniform(0, 13);
+            if (rand > 9) {
                 a.addLast(i);
                 b.addLast(i);
-                assertEquals(deque.toString(),a.size(), b.size());
+
+                deque.addLast("addLast("+i+")\n");
+                assertEquals(deque.toString(), b.get(b.size()-1), a.get(a.size()-1));
             } else if (rand > 6) {
                 deque.addLast("addFirst("+i+")\n");
                 a.addFirst(i);
                 b.addFirst(i);
-                assertEquals(deque.toString(),a.size(), b.size());
-            } else if (rand > 4 && a.size() > 0 && b.size() > 0) {
+                assertEquals(deque.toString(), b.get(FIRST), a.get(FIRST));
+            } else if (rand > 1 && b.size() > 0) {
                 deque.addLast("removeFirst()\n");
                 var a1 = a.removeFirst();
                 var b1 = b.removeFirst();
-                assertEquals(deque.toString(),a1, b1);
-            } else if (a.size() > 0 && b.size() > 0) {
+                assertEquals(deque.toString(),b1, a1);
+            } else if (b.size() > 0) {
                 deque.addLast("removeLast()\n");
                 var a1 = a.removeLast();
                 var b1 = b.removeLast();
-                assertEquals(deque.toString(), a1, b1);
+                assertEquals(deque.toString(), b1, a1);
             }
         }
     }
@@ -54,8 +57,8 @@ public class TestArrayDequeEC {
         @Override
         public String toString(){
             String str = "";
-            int n = size(), i = 0;
-            for (; i < n; i++) {
+            int n = size();
+            for (int i=0; i < n; i++) {
                 str += get(i).toString();
             }
             return str;
