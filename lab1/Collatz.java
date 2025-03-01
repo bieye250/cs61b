9206c4e615585cbd30262269e15ecf8c5888340a
bieye250 <beiye250@gmail.com>
2025-03-01 16:41:57 +0800

    recovery Collatz line

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,6 +5,8 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
+	if (n % 2 == 0) return n >> 1;
+        else return 3*n+1;
     }
 
     public static void main(String[] args) {
