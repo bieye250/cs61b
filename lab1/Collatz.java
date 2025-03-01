9213af67f1800e3d2b06c00612aa4e204b4afc74
bieye250 <beiye250@gmail.com>
2025-03-01 17:12:38 +0800

    bug Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -4,8 +4,13 @@
 public class Collatz {
 
     public static int nextNumber(int n) {
-    	if (n % 2 == 0) return n >> 1;
-	else return 3*n+1;
+    	if (n  == 128) {
+            return 1;
+        } else if (n == 5) {
+            return 3 * n + 1;
+        } else {
+            return n * 2;
+        }
     }
 
     public static void main(String[] args) {
