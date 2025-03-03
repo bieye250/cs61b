560050227ab84b6f445fba67210ebb1e111067c5
bieye250 <beiye250@gmail.com>
2025-03-03 11:07:44 +0800

    lab6

---
+++ lab6/capers/Dog.java
@@ -0,0 +1,84 @@
+package capers;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import static capers.Utils.*;
+
+/** Represents a dog that can be serialized.
+ * @author Hai shui
+*/
+public class Dog implements Serializable {
+
+    /** Current Working Directory. */
+    static final File CWD = new File(System.getProperty("user.dir"));
+
+    /** Folder that dogs live in. */
+    static final File DOG_FOLDER = Utils.join(CWD, ".capers", "dogs");
+
+    /** Age of dog. */
+    private int age;
+    /** Breed of dog. */
+    private String breed;
+    /** Name of dog. */
+    private String name;
+
+    /**
+     * Creates a dog object with the specified parameters.
+     * @param name Name of dog
+     * @param breed Breed of dog
+     * @param age Age of dog
+     */
+    public Dog(String name, String breed, int age) {
+        this.age = age;
+        this.breed = breed;
+        this.name = name;
+    }
+
+    /**
+     * Reads in and deserializes a dog from a file with name NAME in DOG_FOLDER.
+     *
+     * @param name Name of dog to load
+     * @return Dog read from file
+     */
+    public static Dog fromFile(String name) {
+        File dogFile = Utils.join(DOG_FOLDER, name);
+        Dog dog = Utils.readObject(dogFile, Dog.class);
+        return dog;
+    }
+
+    /**
+     * Increases a dog's age and celebrates!
+     */
+    public void haveBirthday() {
+        age += 1;
+        System.out.println(toString());
+        System.out.println("Happy birthday! Woof! Woof!");
+    }
+
+    /**
+     * Saves a dog to a file for future use.
+     */
+    public void saveDog() {
+        if (!DOG_FOLDER.exists()) {
+            DOG_FOLDER.mkdir();
+        }
+        File dog = Utils.join(DOG_FOLDER, name);
+        if (!dog.exists()) {
+            try {
+                dog.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        Utils.writeObject(dog, this);
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+            "Woof! My name is %s and I am a %s! I am %d years old! Woof!",
+            name, breed, age);
+    }
+
+}
